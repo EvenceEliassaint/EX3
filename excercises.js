5514b33b5275cd72c75f81cf14bae35143638dd6
EvenceEliassaint <ev413623@ucf.edu>
2024-02-05 19:48:37 -0500

    Fixed exercises and quotations

--- excercises.js
+++ excercises.js
@@ -1,22 +1,34 @@
 // exercises.js
+let arrays = [[1, 2, 3], [4, 5], [6]];
 export function flattening(list) {
-    return list.reduce((flatArray, subArray) => flatArray.concat(subArray), []);
+    return list.reduce((previousValue, currentValue) => previousValue.concat(currentValue));
   }
+
+  console.log(flattening(arrays))
+
+  // exercises.js
+export function loop(value, test, update, body) {
+    for (let i = value; test(i); i = update(i)) {
+      body(i);
+    }
+  }
+  loop(3, n => n > 0, n => n - 1, console.log);
+  
   // exercises.js
 export function everyLoop(array, test) {
-    for (let element of array) {
-      if (!test(element)) return false;
+    let curResult = true
+    for(let item of array){
+        curResult = curResult && test(item)
     }
-    return true;
-  }
+    return curResult
+    }
   
   export function everySome(array, test) {
-    return array.every(test);
+    return !array.some((value)=>!test(value))
   }
-  // test.js
-import { flattening, loop, everyLoop, everySome } from './exercises.js';
-
-console.log(flattening([[1, 2, 3], [4, 5], [6]]));
-loop(3, n => n > 0, n => n - 1, console.log);
-console.log(everyLoop([1, 3, 5], n => n < 10));
-console.log(everySome([2, 4, 16], n => n < 10));
+  console.log(everyLoop([1, 3, 5], n => n < 10));
+// → true
+console.log(everyLoop([2, 4, 16], n => n < 10));
+// → false
+console.log(everyLoop([], n => n < 10));
+// → true
--- index.js
+++ index.js
@@ -1,18 +1,8 @@
-// index.js
-
-// Array of three quotations
-const quotations = [
-    "The only way to do great work is to love what you do. - Steve Jobs",
-    "In three words I can sum up everything I've learned about life: it goes on. - Robert Frost",
-    "The future belongs to those who believe in the beauty of their dreams. - Eleanor Roosevelt"
+let quotations = [
+    "I wonder what it's like to have a belly button ~ ClapTrap",
+    "I'm gonna eat so many crumpets, it's gonna be a crump-pocalypse! ~ Tinya Tina",
+    "There's no barbeque until I say there's a barbeque. ~ Krieg"
   ];
   
-  // Function to get a random quotation
-  function getRandomQuotation() {
-    const randomIndex = Math.floor(Math.random() * quotations.length);
-    return quotations[randomIndex];
-  }
-  
-  // Print a random quotation to the console
-  console.log(getRandomQuotation());
+  console.log(quotes[Math.floor(Math.random()*3)])
   
